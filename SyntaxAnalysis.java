1a1c05a2d448b4275d9a5d10f5b565b2e50e8078
Luqman <luqmanazri@student.usm.my>
2023-11-16 16:27:51 +0800

    lol

--- SyntaxAnalysis.java
+++ SyntaxAnalysis.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 class main {
-    // luqa hehe
+    // luqa hehe 111
     static List<String> Tokenization(String input, List<String> v) {
         StringBuilder token = new StringBuilder();
 
